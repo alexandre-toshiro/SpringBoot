2d655da67f5492fb784630e98cf1570794f4e05a
Toshiro <toshiro.lyn@gmail.com>
2020-09-27 18:37:57 -0300

    Utilizando o padrão DTO

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,22 +3,21 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController // Já assume que todos os métodos da classe retornarão no body .
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody // Sem página, o retorna do método irá diretamente para o navegador
-	public List<Topico> lista(){
+	public List<TopicoDto> lista(){
 		Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 	
 }
