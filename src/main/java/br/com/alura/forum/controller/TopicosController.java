416d2005e89d61411ae936c22c662cd51b86e77b
Toshiro <toshiro.lyn@gmail.com>
2020-09-28 15:55:33 -0300

    Consulta do BD com filtro / nomeclatura findBy do Spring

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -10,17 +10,21 @@ import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
-
-
 @RestController // Já assume que todos os métodos da classe retornarão no body .
 public class TopicosController {
 	@Autowired
 	private TopicoRepository topicoRepository;
 
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-				List<Topico> topicos = topicoRepository.findAll(); //Encontra todos os registos no banco.
+	public List<TopicoDto> lista(String nomeCurso) {// Recebe o parâmetro que será utilizado na uri.
+		if(nomeCurso == null) {
+		List<Topico> topicos = topicoRepository.findAll(); // Encontra todos os registos no banco.
 		return TopicoDto.converter(topicos);// converte a lista dos achados em banco para TopicoDto.
+
+		}else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
 	}
-	
+
 }
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,10 +1,22 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
-public interface TopicoRepository extends JpaRepository<Topico, Long>{
-																							//entidade, tipo da chave primária				
+public interface TopicoRepository extends JpaRepository<Topico, Long>{//entidade, tipo da chave primária
+
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	// Criar um método com esse nomeclatura "findBy", para o Spring de forma automática montar a query.
+	/* A entidade topico possui um relacionamento com a entidade Curso, que possui o atributo nome
+	 * Para buscar o curso pelo nome, apenas seguir a ordem do relacionamento 
+	 * Topico>Curso>Nome -> findByCurso_Nome
+	 * utilizar o "_" para tirar ambiguidade entre atributo e relacionamento
+	 * No caso o Spring entende como relacionamento.
+	 * 	 * */
+	
+																											
 
 }
