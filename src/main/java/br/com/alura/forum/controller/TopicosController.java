d1541aa7e72ef17dfaeaeed41288eaedb3cc6f99
Toshiro <toshiro.lyn@gmail.com>
2020-09-28 20:38:08 -0300

    Cadastro de topicos no banco de dados

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,30 +1,50 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.forum.TopicoForm;
 import br.com.alura.forum.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController // Já assume que todos os métodos da classe retornarão no body .
+@RequestMapping("/topicos")
 public class TopicosController {
 	@Autowired
 	private TopicoRepository topicoRepository;
 
-	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(String nomeCurso) {// Recebe o parâmetro que será utilizado na uri.
-		if(nomeCurso == null) {
-		List<Topico> topicos = topicoRepository.findAll(); // Encontra todos os registos no banco.
-		return TopicoDto.converter(topicos);// converte a lista dos achados em banco para TopicoDto.
+	@Autowired
+	private CursoRepository cursoRepository;
 
-		}else {
+	@GetMapping
+	public List<TopicoDto> lista(String nomeCurso) {// Recebe o parâmetro que será utilizado na uri.
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll(); // Encontra todos os registos no banco.
+			return TopicoDto.converter(topicos);// converte a lista dos achados em banco para TopicoDto.
+
+		} else {
 			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
 	}
 
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {// Pegar do corpo da req e n da url.
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
 }
