d469c99a024094e34dcbd03f04ca1667864b79a3
Toshiro <toshiro.lyn@gmail.com>
2020-09-29 22:51:51 -0300

    Exclusao de úm topico / Finalizacao do CRUD

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class TopicosController {
 	}
 
 	@PostMapping
+	@Transactional // para disparar o commit no banco de dados.
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {// Pegar do corpo da req e n da url.
 																								// valid roda as validações do B.Validation					
 		Topico topico = form.converter(cursoRepository);
@@ -68,7 +70,14 @@ public class TopicosController {
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
 		Topico topico = form.atualizar(id, topicoRepository);
 		
-		return ResponseEntity.ok(new TopicoDto(topico));
+		return ResponseEntity.ok(new TopicoDto(topico));	
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional // para disparar o commit no banco de dados.
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		topicoRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 		
 	}
 }
